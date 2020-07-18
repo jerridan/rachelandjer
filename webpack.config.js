434abcd66b3011f9e2e31a93a6520a999163c233
Jerridan Quiring <jerridan@gmail.com>
2020-07-18 10:50:54 -0400

    Get 'Hello, world' message to display

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,22 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
+  devtool: "eval-cheap-source-map",
   module: {
-    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+    rules: [
+      {
+        test: /\.(js?)$/,
+        exclude: /node_modules/,
+        use: { loader: "babel-loader" },
+      },
+    ],
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
   resolve: {
     extensions: [".js", ".jsx"],
   },
