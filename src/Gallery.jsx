b45b38ab0fa685768e58206de1919c501ffb8662
Jerridan Quiring <jerridan@gmail.com>
2020-07-20 21:33:06 -0400

    Create proof of concept Gallery that can request and display thumbnails from dropbox

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Carousel from "@brainhubeu/react-carousel";
 import "@brainhubeu/react-carousel/lib/style.css";
+import { Dropbox } from "dropbox";
+import isEmpty from "lodash/isEmpty";
 
 export default function Gallery() {
+  const [thumbnails, setThumbnails] = useState([]);
+
+  const dbx = new Dropbox({
+    accessToken: process.env.DROPBOX_ACCESS_TOKEN,
+  });
+
+  useEffect(() => {
+    const fetchThumbnails = async () => {
+      const result = await dbx.filesGetThumbnailBatch({
+        entries: [
+          {
+            path: "/img_20200611_205538.jpg",
+            format: "jpeg",
+            size: "w128h128",
+          },
+        ],
+      });
+
+      setThumbnails(result.entries);
+    };
+
+    fetchThumbnails();
+  }, []);
+
+  if (isEmpty(thumbnails)) return null;
+
   return (
     <Carousel arrows dots>
       <div>
-        <h3>1</h3>
+        <img src={`data:image/jpeg;base64, ${thumbnails[0].thumbnail}`} />
       </div>
       <div>
         <h3>2</h3>
