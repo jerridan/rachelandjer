57e28a29f4ebae0aaa2b48b8e2320d9aa203097a
Jerridan Quiring <jerridan@gmail.com>
2020-09-11 21:41:05 -0400

    Create Ceremony page

---
+++ src/Ceremony.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Ceremony() {
+  return <div>Ceremony Page</div>;
+}
--- src/Router.jsx
+++ src/Router.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Timeline from "./Timeline";
 import Home from "./Home";
+import Ceremony from "./Ceremony";
 
 export default function RouterComponent() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/ceremony" component={Ceremony} />
         <Route path="/timeline" component={Timeline} />
       </Switch>
     </BrowserRouter>
