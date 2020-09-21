78cd71e887fda533f75b26b8e02addcb41ca3707
Jerridan Quiring <jerridan@gmail.com>
2020-09-20 21:55:38 -0400

    Extract Background to a separate shared component

--- src/Ceremony/Ceremony.jsx
+++ src/Ceremony/Ceremony.jsx
@@ -1,15 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import backgroundImage from "../images/leafy_green_bg_2400x1600.jpg";
-
-const Background = styled.div`
-  position: absolute;
-  height: 100vh;
-  width: 100%;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-image: url(${backgroundImage});
-`;
+import Background from "../Background";
 
 const Welcome = styled.div`
   text-align: center;
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,20 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import backgroundImage from "../images/leafy_green_bg_2400x1600.jpg";
+import Background from "../Background";
 
 export default function Home() {
-  const Background = styled.div`
-    position: absolute;
-    height: 100vh;
-    width: 100%;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-image: url(${backgroundImage});
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  `;
-
   const Names = styled.p`
     margin: 10px;
     letter-spacing: 0.3em;
