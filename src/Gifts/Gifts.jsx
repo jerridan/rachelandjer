f8de4d5a81e20b68e82d176d3ecab8c98bb9d33d
Jerridan Quiring <jerridan@gmail.com>
2020-09-27 17:53:05 -0400

    Move styled components out of the main component definition

--- src/Gifts/Gifts.jsx
+++ src/Gifts/Gifts.jsx
@@ -4,6 +4,60 @@ import Navigation from "../Navigation/Navigation";
 import styled from "styled-components";
 import "./kwesFormOverrides.css";
 
+const Content = styled.div`
+  background-color: #f9f9f9;
+  text-align: center;
+  font-family: Lora, serif;
+  color: #373d3f;
+  padding: 50px;
+`;
+
+const Title = styled.p`
+  margin-top: 25px;
+  margin-bottom: 25px;
+  letter-spacing: 0.3em;
+  font-family: Tangerine, serif;
+  font-size: 50px;
+`;
+
+const Details = styled.p`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  letter-spacing: 0.1em;
+  font-size: 16px;
+  max-width: 750px;
+  text-align: left;
+  line-height: 1.25em;
+`;
+
+const Form = styled.form`
+  text-align: left;
+`;
+
+const Field = styled.div`
+  display: block;
+  padding-bottom: 10px;
+`;
+
+const Label = styled.label`
+  width: 150px;
+  display: inline-block;
+  vertical-align: top;
+  padding-top: 5px;
+`;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #373d3f;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #373d3f;
+`;
+
 export default function Gifts() {
   const kwesScript = document.createElement("script");
 
@@ -11,60 +65,6 @@ export default function Gifts() {
   kwesScript.setAttribute("charset", "utf-8");
 
   document.head.appendChild(kwesScript);
-
-  const Content = styled.div`
-    background-color: #f9f9f9;
-    text-align: center;
-    font-family: Lora, serif;
-    color: #373d3f;
-    padding: 50px;
-  `;
-
-  const Title = styled.p`
-    margin-top: 25px;
-    margin-bottom: 25px;
-    letter-spacing: 0.3em;
-    font-family: Tangerine, serif;
-    font-size: 50px;
-  `;
-
-  const Details = styled.p`
-    margin-top: 20px;
-    margin-bottom: 20px;
-    letter-spacing: 0.1em;
-    font-size: 16px;
-    max-width: 750px;
-    text-align: left;
-    line-height: 1.25em;
-  `;
-
-  const Form = styled.form`
-    text-align: left;
-  `;
-
-  const Field = styled.div`
-    display: block;
-    padding-bottom: 10px;
-  `;
-
-  const Label = styled.label`
-    width: 150px;
-    display: inline-block;
-    vertical-align: top;
-    padding-top: 5px;
-  `;
-
-  const TextArea = styled.textarea`
-    width: 100%;
-    padding: 5px;
-    border: 1px solid #373d3f;
-  `;
-
-  const Input = styled.input`
-    width: 100%;
-    padding: 5px;
-    border: 1px solid #373d3f;
-  `;
 
   return (
     <Background>
