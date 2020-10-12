18b86edb18cb466170046a40b0dd48278c54185d
Jerridan Quiring <jerridan@gmail.com>
2020-10-12 15:31:44 -0400

    Resolve warnings created by styled components incorrectly implemented inside of another component

--- src/Layout/Navigation/Link.jsx
+++ src/Layout/Navigation/Link.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-export default function Link({ children, to }) {
-  const NavListItem = styled.li`
-    list-style: none;
-    height: 45px;
-    font-size: 14px;
+const NavListItem = styled.li`
+  list-style: none;
+  height: 45px;
+  font-size: 14px;
 
-    a {
-      text-decoration: none;
-      color: inherit;
-      padding-bottom: 5px;
-      &:hover {
-        color: #838a62;
-      }
+  a {
+    text-decoration: none;
+    color: inherit;
+    padding-bottom: 5px;
+    &:hover {
+      color: #838a62;
     }
-  `;
+  }
+`;
 
+export default function Link({ children, to }) {
   return (
     <NavListItem>
       <NavLink
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,24 +2,24 @@ import React from "react";
 import styled from "styled-components";
 import Layout from "../../Layout/Layout";
 
+const Names = styled.p`
+  margin: 10px;
+  letter-spacing: 0.3em;
+  font-family: Tangerine, serif;
+  font-size: 60px;
+`;
+
+const Details = styled.p`
+  letter-spacing: 0.1em;
+  font-size: 16px;
+`;
+
+const Content = styled.div`
+  text-align: center;
+  height: 200px;
+`;
+
 export default function Home() {
-  const Names = styled.p`
-    margin: 10px;
-    letter-spacing: 0.3em;
-    font-family: Tangerine, serif;
-    font-size: 60px;
-  `;
-
-  const Details = styled.p`
-    letter-spacing: 0.1em;
-    font-size: 16px;
-  `;
-
-  const Content = styled.div`
-    text-align: center;
-    height: 200px;
-  `;
-
   return (
     <Layout>
       <Content>
