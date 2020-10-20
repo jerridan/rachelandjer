4268df15ce80e6fe49eac473c6fdef81df2979d6
Jerridan Quiring <jerridan@gmail.com>
2020-10-19 22:39:54 -0400

    Make embedded livestream more mobile friendly

--- src/pages/Ceremony/Ceremony.jsx
+++ src/pages/Ceremony/Ceremony.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import { useMediaQuery } from "react-responsive";
 import theme from "../../theme";
 import Layout from "../../Layout/Layout";
 import Content from "../../components/Content";
 import Details from "../../components/Details";
 import Title from "../../components/Title";
+import { extraSmall } from "../../breakpoints";
 
 const Date = styled.p`
   letter-spacing: 0.1em;
@@ -20,6 +22,8 @@ const ExternalLink = styled.a`
 `;
 
 export default function Ceremony() {
+  const isTinyScreen = useMediaQuery({ maxWidth: extraSmall });
+
   return (
     <Layout>
       <Content>
@@ -29,7 +33,7 @@ export default function Ceremony() {
         <Details>
           <iframe
             title="ceremony-livestream"
-            width="560"
+            width={isTinyScreen ? 300 : 560}
             height="315"
             src="https://www.youtube-nocookie.com/embed/ruIyVCSjsnY"
             frameBorder="0"
