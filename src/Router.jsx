6b3c8b3ecfd0be188470b4a5ad9b84d7f987a5f6
Jerridan Quiring <jerridan@gmail.com>
2020-11-30 09:45:56 -0500

    Remove Gifts page

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,8 +3,6 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Timeline from "./pages/Timeline/Timeline";
 import Home from "./pages/Home/Home";
 import Ceremony from "./pages/Ceremony/Ceremony";
-import Gifts from "./pages/Gifts/Gifts";
-import MessageSent from "./pages/MessageSent/MessageSent";
 import OurStory from "./pages/OurStory/OurStory";
 
 export default function RouterComponent() {
@@ -13,9 +11,7 @@ export default function RouterComponent() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/ceremony" component={Ceremony} />
-        <Route path="/gifts" component={Gifts} />
         <Route path="/timeline" component={Timeline} />
-        <Route path="/message-sent" component={MessageSent} />
         <Route path="/our-story" component={OurStory} />
       </Switch>
     </BrowserRouter>
